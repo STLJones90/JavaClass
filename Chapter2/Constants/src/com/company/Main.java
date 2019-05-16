ee4484439c485ee3e8536eb698bc8fee1c0af385
STLJones90 <cacmustangs09@yahoo.com>
2019-05-15 21:10:33 -0500

    ATM Program

--- Chapter2/Constants/src/com/company/Main.java
+++ Chapter2/Constants/src/com/company/Main.java
@@ -1,24 +1,26 @@
 package com.company;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
 // CONSTANT SCORE VALUE
-final int TOUCHDOWN, CONVERSION, FIELDGOAL;
+        final int TOUCHDOWN, CONVERSION, FIELDGOAL;
 
-          TOUCHDOWN = 6;
-          CONVERSION = 1;
-          FIELDGOAL= 3;
+        TOUCHDOWN = 6;
+        CONVERSION = 1;
+        FIELDGOAL = 3;
 
-          //INPUT NUMBER OF TIMES EACH WAS SCORED
-          //PAT (point after touchdown) is "conversion"
-          int td, pat, fg, total;
+        //INPUT NUMBER OF TIMES EACH WAS SCORED
+        //PAT (point after touchdown) is "conversion"
+        int td, pat, fg, total;
 
-          td = 4 * TOUCHDOWN;
-          pat = 3 * CONVERSION;
-          fg = 2 * FIELDGOAL;
-          total = (td + pat + fg);
+        td = 4 * TOUCHDOWN;
+        pat = 3 * CONVERSION;
+        fg = 2 * FIELDGOAL;
+        total = (td + pat + fg);
 
         System.out.println("Score: " + total);
 
