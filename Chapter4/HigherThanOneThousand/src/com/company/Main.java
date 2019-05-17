8f7ea861dae1ff48481223c0485adece9bfd5589
STLJones90 <cacmustangs09@yahoo.com>
2019-05-17 15:31:34 -0500

    Higher than 1000

--- Chapter4/HigherThanOneThousand/src/com/company/Main.java
+++ Chapter4/HigherThanOneThousand/src/com/company/Main.java
@@ -1,26 +1,39 @@
 package com.company;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
+        java.util.Scanner userInput = new java.util.Scanner(System.in);
 
+        System.out.println("Enter a number");
 
-
-
-
-
-
-
-
-
+        int userNumber = userInput.nextInt();
+        int counter = 1;
 
 
+        while (userNumber <= 1000)
+        {
 
+            System.out.println("Enter another number");
 
+            userNumber += userInput.nextInt();
+            counter++;
 
+        }
 
+            System.out.println("You reached a total of " + userNumber + " to finally obtain a total greater than 1000");
 
+        if (counter==1)
+        {
+            System.out.println("You entered " + counter + " number");
+        }
+        else
+        {
+            System.out.println("You entered " + counter + " numbers");
+        }
 
 
 
