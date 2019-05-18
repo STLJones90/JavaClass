5aa30ae9edccfd80c26b958134e9f891b2032e35
STLJones90 <cacmustangs09@yahoo.com>
2019-05-18 15:17:50 -0500

    CountTheNumbers

---
+++ Chapter4/CountTheNumbers/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        java.util.Scanner userInput = new java.util.Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int num1 = userInput.nextInt();
+        int num2 = userInput.nextInt();
+
+        while (num1<num2)
+        {
+            System.out.println(num1);
+            num1++;
+
+        }
+
+        while (num1>num2)
+        {
+            System.out.println(num1);
+            num1--;
+        }
+
+        if (num1==num2);
+        System.out.println(num2);
+        
+    }
+}
