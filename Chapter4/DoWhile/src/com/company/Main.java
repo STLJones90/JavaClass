d7db70d97abeedd24afb1a0ad06e25929658dd5a
STLJones90 <cacmustangs09@yahoo.com>
2019-05-20 10:16:08 -0500

    Do While

---
+++ Chapter4/DoWhile/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+int num = 0;
+
+do
+{
+    System.out.println("DoWhile Countup: " + num);
+    num += 10;
+}
+
+while (num<100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
