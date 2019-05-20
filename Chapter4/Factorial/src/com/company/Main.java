ddbba93e52785771120dfb7c8f402b7bde186e4f
STLJones90 <cacmustangs09@yahoo.com>
2019-05-20 08:38:35 -0500

    Factorial

---
+++ Chapter4/Factorial/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        int int1 = 5;
+        int int2 = 4;
+
+
+        while (int2 > 1)
+        {
+            int1 = (int1 * int2);
+            System.out.println(int1);
+            int2--;
+
+        }
+
+
+    }
+}
