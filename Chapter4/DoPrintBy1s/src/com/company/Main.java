cce02d2aeee459d681ca93292eebc53c632ccf7e
STLJones90 <cacmustangs09@yahoo.com>
2019-05-20 10:22:17 -0500

    Do Print by Ones

---
+++ Chapter4/DoPrintBy1s/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+        int num2 = 10;
+
+        do
+        {
+            System.out.println(num);
+            num++;
+        }
+while (num<=10);
+
+do
+{
+    System.out.println(num2);
+    num2--;
+}
+
+while (num2>0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
