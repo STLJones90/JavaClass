87f4f51ff756ef50c785cb07d6abe335a437d0c8
STLJones90 <cacmustangs09@yahoo.com>
2019-05-21 10:32:06 -0500

    Hello World

---
+++ Chapter5/HelloWorld/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.PrimitiveIterator;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        sayHelloWorld();
+    }
+
+    private static void sayHelloWorld()
+    {
+        System.out.println("Hello, world");
+    }
+}
+
