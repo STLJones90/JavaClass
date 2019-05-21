e3d07dc21f14c358e5e51bc7cb03f3a37219c960
STLJones90 <cacmustangs09@yahoo.com>
2019-05-21 10:57:59 -0500

    Sums

---
+++ Chapter5/Sums/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        printSum(4, 6);
+        printSum(7, 2);
+    }
+
+    private static void printSum(int int1, int int2)
+    {
+        System.out.println(int1 + int2);
+    }
+}
