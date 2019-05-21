31b2d29f673cdd4d4abdcfec88757101872c6ec1
STLJones90 <cacmustangs09@yahoo.com>
2019-05-21 10:47:25 -0500

    Space Alien Shopping Trip

---
+++ Chapter5/SpaceAlienShoppingTrip/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        printHello();
+        goShopping();
+        goShopping();
+        goShopping();
+        printGoodbye();
+    }
+
+    private static void printHello()
+    {
+        System.out.println("Hello, Earthlings");
+    }
+
+    private static void printGoodbye()
+    {
+        System.out.println("Goodbye Earthlings");
+    }
+
+    private static void goShopping()
+    {
+        System.out.println("Shopping like it's 1999!");
+    }
+
+
+}
