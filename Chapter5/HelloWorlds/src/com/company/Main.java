a36e3029f4eefe193e22f6a47dbec00184702820
STLJones90 <cacmustangs09@yahoo.com>
2019-05-21 10:36:00 -0500

    Hello Worlds

---
+++ Chapter5/HelloWorlds/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int count = 0;
+        while (count<10)
+        {
+            sayHelloWorld();
+            count++;
+        }
+    }
+    public static void sayHelloWorld()
+    {
+        System.out.println("Hello, World");
+
+    }
+}
