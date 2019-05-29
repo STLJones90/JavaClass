0ef243d86eff7381b888b6d28c5cd9246df6cf26
STLJones90 <cacmustangs09@yahoo.com>
2019-05-29 10:33:15 -0500

    Date Until

---
+++ Chapter6/DateUntil/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+Date date = new Date(2, 10, 2020);
+
+        System.out.println(date.getFormattedDate());
+
+
+
+    }
+}
